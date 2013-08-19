40d64503e03aa16f8b5c7e970175eef0d7a237fb
Vitiaaap29 <viktor.kyrilovich@gmail.com>
2013-08-19 11:49:10 +0300

    before page 110 Alige RoR

---
+++ app/models/product.rb
@@ -0,0 +1,9 @@
+class Product < ActiveRecord::Base
+	validates :description, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\z},
+		message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
+	}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'e4a4fd4d210935ac5b7bb1f6188d141973960a3df57a292c9fa781e93bd250e5389746571346424f549067f5be0151867e80b9f724eee03cf6e513cca1d8adb5'
